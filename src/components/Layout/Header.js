22533af96d607de8e98df599158320e268c31dd6
Cannon-Cloud <clarence@cannon.cloud>
2021-07-12 16:07:52 -0700

    Header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,1 +1,20 @@
 import React from "react";
+
+import headerImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+
+const Header = (props) => {
+  return (
+    <React.Fragment>
+      <header className={classes.header}>
+        <h1>Food Test (Can Change)</h1>
+        <button>Cart</button>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={headerImage} alt="A table full of food you want" />
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Header;
