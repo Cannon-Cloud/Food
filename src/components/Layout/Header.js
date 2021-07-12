d22b91c8c8c8364138828762aa237d9bd4487651
Cannon-Cloud <clarence@cannon.cloud>
2021-07-12 16:31:39 -0700

    Added "Cart" and basic Meals

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import HeaderCartButton from "./HeaderCartButton";
 import headerImage from "../../assets/meals.jpg";
 import classes from "./Header.module.css";
 
@@ -8,7 +9,7 @@ const Header = (props) => {
     <React.Fragment>
       <header className={classes.header}>
         <h1>Food Test (Can Change)</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={classes["main-image"]}>
         <img src={headerImage} alt="A table full of food you want" />
